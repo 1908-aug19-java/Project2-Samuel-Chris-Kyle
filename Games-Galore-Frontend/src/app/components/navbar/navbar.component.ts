8e028a5dc797ce0b8710ce4b51932dd7a81ce4b7
Kyle Reimer <reimerkp@gmail.com>
2019-09-25 16:22:08 -0400

    Finished Nav No Routes

--- Games-Galore-Frontend/src/app/components/navbar/navbar.component.ts
+++ Games-Galore-Frontend/src/app/components/navbar/navbar.component.ts
@@ -7,8 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  homeTab = false;
+  wishlistTab = false;
+  profileTab = false;
   constructor() { }
 
+  listClick(event) {
+    this.homeTab = false;
+    this.wishlistTab = false;
+    this.profileTab = false;
+    switch (event) {
+      case "home":
+        this.homeTab = true;
+        break;
+      case "profile":
+        this.profileTab = true;
+        break;
+      case "wishlist":
+        this.wishlistTab = true;
+        break;
+    }
+  }
   ngOnInit() {
   }
 
