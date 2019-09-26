271a38afaaa88a3fa1650a9011d2d31bda2d3953
chrstphrclmn <chrstphrclmn@gmail.com>
2019-09-26 15:30:35 -0400

    Commit Some stuff

---
+++ Games-Galore-Frontend/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { LoginComponent } from '../components/login/login.component';
+import { RegisterComponent } from '../components/register/register.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: DashboardComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: '',
+        component: LoginComponent,
+        outlet: 'apple'
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ],
+    declarations: []
+})
+export class AppRoutingModule { }
