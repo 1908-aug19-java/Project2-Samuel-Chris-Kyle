20cfd8df631a962756660b93268f3dc35e62baae
chrstphrclmn <chrstphrclmn@gmail.com>
2019-09-27 11:46:16 -0400

    Added some more stuff

--- Games-Galore-Frontend/src/app/app-routing/app-routing.module.ts
+++ Games-Galore-Frontend/src/app/app-routing/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
 import { LoginComponent } from '../components/login/login.component';
 import { RegisterComponent } from '../components/register/register.component';
+import { RouterComponent } from '../components/router/router.component';
 
 const routes: Routes = [
     {
@@ -17,6 +18,10 @@ const routes: Routes = [
     {
         path: 'register',
         component: RegisterComponent
+    },
+    {
+        path: 'router',
+        component: RouterComponent
     }
 ];
 
