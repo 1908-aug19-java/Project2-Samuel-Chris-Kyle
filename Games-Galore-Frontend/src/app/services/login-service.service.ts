bcea5ad5a74374a410e706baba82c3748d8e8f9f
Kyle Reimer <reimerkp@gmail.com>
2019-10-01 11:06:51 -0400

    Completed adding login logic
    Once logged in sends user to the homepage and currently prints out the session variable of their auth token.

---
+++ Games-Galore-Frontend/src/app/services/login-service.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders} from '@angular/common/http';
+import { User } from '../components/models/user';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginServiceService {
+
+  url:string = "http://ec2-54-80-50-16.compute-1.amazonaws.com:8081/gamesgalore/login?"
+  constructor(private http: HttpClient) { }
+
+  loginUser(userName: string, password: string): Observable<Object>{
+    console.log("Username: " + userName + ", Password: " + password );
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/x-www-form-urlencoded'
+      })
+      
+    };
+    return this.http.post<any>(this.url + "accountPassword="+password + "&accountUsername=" + userName, httpOptions,{observe: 'response' as 'body'});
+  }
+
+}
